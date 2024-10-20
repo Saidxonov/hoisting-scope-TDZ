5175e342334d83e5d0d2dc675b00182f19dc789d
Saidburxon <saidburxon@gmail.com>
2024-10-20 13:47:34 +0500

    2 misol ishlandi

--- js/app.js
+++ js/app.js
@@ -16,3 +16,16 @@
 //   return value;
 // });
 // console.log(sum);
+
+// ### Masala 2: `find` va `map` metodlari bilan talabalarni topish
+
+// let students = [
+//   { ismi: "Saidburxon", age: 14 },
+//   { name: "Sardor", age: 21 },
+//   { name: "Hitler", age: 138 },
+// ];
+// let res = students.find(function (value) {
+//   if (value.age > 20) {
+//     console.log(value);
+//   }
+// });
