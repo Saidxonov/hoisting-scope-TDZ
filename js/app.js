e0d455d44c9706da7ae770a22aa1ee302dcf346f
Saidburxon <saidburxon@gmail.com>
2024-10-20 11:53:39 +0500

    birinchi masala ishlandi

--- js/app.js
+++ js/app.js
@@ -1,58 +1,18 @@
-// // regular function
-// function add(a, b) {
-//     return a + b;
-// }
+//--------------------------------------> Massivlarga oid <---------------------------
 
-// //express function
-// const add = function(a, b) {
-// return a + b;
-// }
+// ### Masala 1: `map` va `filter` metodlari bilan alifbo tartibini o'zgartirish
 
-// // arrow function
-// const add = (a, b) => {
-//     return a + b;
-// }
+// let arr = ["hello ", "world", "mersedes-benz", "bmw", "toyota", "lacetti"];
+// let sum1 = arr.filter(function (value) {
+//   if (value.length > 5) {
+//     return value;
+//   }
+// });
 
-//IIFE
-// (function() {
-//     var test = "salom";
-
-//     function abs() {
-//         console.log("abs");
-//     }
-// })();
-
-// object
-// let user = {
-//     name: "John",
-//     age: 56,
-//     phones: ["+998123456789", "+7 97249723"],
-//     email: "test@gmail.com",
-//     family: {
-//         father: "Omerbey",
-//         mother: "Alise",
-//     },
-//     isMarried: false,
-// };
-// //chiqarish usullari
-// // . orqali
-// console.log(user.email);
-// // [""] orqali
-// console.log(user["phones"]);
-// // .ichma-ich qilish
-// console.log(user.family.father);
-
-let arr = ["hello ", "world", "mersades", "bmw", "matiz", "lacetti"];
-let sum1 = arr.filter(function (value) {
-  if (value.length > 5) {
-    return value;
-  }
-});
-
-let sum = sum1.map(function (value) {
-  if (value.length > 5) {
-    return value.toUpperCase();
-  }
-  return value;
-});
-console.log(sum);
+// let sum = sum1.map(function (value) {
+//   if (value.length > 5) {
+//     return value.toUpperCase();
+//   }
+//   return value;
+// });
+// console.log(sum);
